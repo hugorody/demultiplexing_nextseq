d5725363e3d7020b2703ebcfba251bd6ed3209fb
Hugo Rody <hugo.rody@unifesp.br>
2017-06-13 08:53:18 -0300

    Update merge_fastq_demultiplexing.py

--- merge_fastq_demultiplexing.py
+++ merge_fastq_demultiplexing.py
@@ -4,8 +4,9 @@ import sys
 from Bio import SeqIO
 import glob, os
 
-file1 = sys.argv[1]
-dir1 = sys.argv[2] #output directory, where the fq files to be merged are and output of this script will be written
+file1 = sys.argv[1] #list containing individuals ids separated by breaklines
+dir1 = sys.argv[2] #input directory, where the fq files
+dir2 = sys.argv[3] #output directory, to write the fastq files
 
 #create a list of individuals based on file1
 listindividuals = []
@@ -18,14 +19,14 @@ with open(file1) as set1:
 for i in listindividuals:
 
     searchfile = i+".fq"
-    outputname = str(dir1)+str(i)+".fastq"
+    outputname = str(dir2)+str(i)+".fastq" #outputname is set to write in the output directory 2
     output = open(outputname,'w')
 
     lisftoffiles = [] #list of files that match to individual
     
-    os.chdir(dir1) #list all files in directory 1
+    os.chdir(dir1) #list all files in the input directory 1
     for file in glob.glob("*.fq"): #and filter by *.fq
-        if searchfile in file: #if search file, which is with .fq, is on any of the files from dir 1
+        if searchfile in file: #if search file, which is with .fq, is on any of the files from input dir 1
             lisftoffiles.append(file) #append the name of the file in list of files
     
     for j in lisftoffiles: #for each of the files in list of files that match to the individual
